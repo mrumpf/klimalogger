af407139444a62df74d4730038e652d4539a5a62
Michael Rumpf <michael@rumpfonline.de>
2012-08-03 00:39:05 +0200

    Added documentation Use the Raspberry Pi commands instead of the serial line flag lines.

---
+++ README.md
@@ -0,0 +1,6 @@
+NOTE: This is currently under development and it does not work yet!
+
+# Hardware
+The schematics for the connection between DB9 and the GPIO pins of the Raspberry Pi can be found here:
+[![CircuitLab Schematic 65xvhy](https://www.circuitlab.com/circuit/65xvhy/screenshot/540x405/)](https://www.circuitlab.com/circuit/65xvhy/rpi-klimalogger/)
+
--- linux3600.c
+++ linux3600.c
@@ -16,6 +16,72 @@
 #include "mcdelay.h"
 #include "mcdelay.c"
 
+#define PAGE_SIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+int  mem_fd;
+char *gpio_mem, *gpio_map;
+char *spi0_mem, *spi0_map;
+
+
+// I/O access
+volatile unsigned *gpio;
+
+#define BCM2708_PERI_BASE        0x20000000
+#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+//
+// Set up a memory regions to access GPIO
+//
+void setup_io()
+{
+   /* open /dev/mem */
+   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+      printf("can't open /dev/mem \n");
+      exit (-1);
+   }
+
+   /* mmap GPIO */
+
+   // Allocate MAP block
+   if ((gpio_mem = malloc(BLOCK_SIZE + (PAGE_SIZE-1))) == NULL) {
+      printf("allocation error \n");
+      exit (-1);
+   }
+
+   // Make sure pointer is on 4K boundary
+   if ((unsigned long)gpio_mem % PAGE_SIZE)
+     gpio_mem += PAGE_SIZE - ((unsigned long)gpio_mem % PAGE_SIZE);
+
+   // Now map it
+   gpio_map = (unsigned char *)mmap(
+      (caddr_t)gpio_mem,
+      BLOCK_SIZE,
+      PROT_READ|PROT_WRITE,
+      MAP_SHARED|MAP_FIXED,
+      mem_fd,
+      GPIO_BASE
+   );
+
+   if ((long)gpio_map < 0) {
+      printf("mmap error %d\n", (int)gpio_map);
+      exit (-1);
+   }
+
+   // Always use volatile pointer!
+   gpio = (volatile unsigned *)gpio_map;
+
+
+}
+
 /********************************************************************
  * open_weatherstation, Windows version
  *
@@ -34,6 +100,29 @@ WEATHERSTATION open_weatherstation (char *device) {
   //calibrate nanodelay function
   microdelay_init(1);
 
+  // Set up gpi pointer for direct register access
+  setup_io();
+
+  // Switch GPIO 7..11 to output mode
+
+ /************************************************************************\
+  * You are about to change the GPIO settings of your computer.          *
+  * Mess this up and it will stop working!                               *
+  * It might be a good idea to 'sync' before running this program        *
+  * so at least you still have your code changes written to the SD-card! *
+ \************************************************************************/
+
+  // Set GPIO pins 7-11 to output
+  for (g=7; g<=11; g++)
+  {
+    INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
+    OUT_GPIO(g);
+  }
+
+	
+
+	
+	
   //Setup serial port
   if ((ws = open(device, O_RDWR | O_NOCTTY)) < 0)
   {
